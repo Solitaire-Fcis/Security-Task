e6e4b6474ac6aa5ae78dd07ccc9e89cfe936d431
Youssefanter <yooiialy@gmail.com>
2022-03-02 18:26:24 +0200

    first commit

---
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+        
+        public string Encrypt(string plainText, int key)
+        {
+            //throw new NotImplementedException();
+            int C_T_index = 0;
+            string C_T = "";
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                for (int j = 0; j <= 25; j++)
+                {
+                    if (plainText[i] == alphabet[j])
+                    {
+                        C_T_index = (j + key) % 26;
+                        break;
+                    }
+                }
+                C_T += alphabet[C_T_index].ToString().ToUpper();
+            }
+            return C_T;
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            //throw new NotImplementedException();
+            string P_T = ""; int P_T_index = 0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                for (int j = 0; j <= 25; j++)
+                {
+                    if (cipherText[i] == alphabet[j])
+                    {
+                        P_T_index = (j - key) % 26;
+                        break;
+                    }
+                }
+                if (P_T_index < 0)
+                {
+                    P_T_index += alphabet.Length;
+                }
+                P_T += alphabet[P_T_index].ToString().ToLower();
+            }
+            return P_T;
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            //throw new NotImplementedException();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            int key, P_T_index=0, C_T_index=0;
+            cipherText = cipherText.ToLower();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (plainText[0]==alphabet[i])
+                {
+                    P_T_index = i;
+                }
+                if (cipherText[0]==alphabet[i])
+                {
+                    C_T_index = i;
+                }
+            }
+            key = C_T_index - P_T_index;
+            if (key<0)
+            {
+                key += alphabet.Length;
+            }
+            return key;
+        }
+    }
+}
