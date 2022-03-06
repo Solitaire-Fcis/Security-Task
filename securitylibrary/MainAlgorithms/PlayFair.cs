c6a2f6d3520dac31a49594dddea3d1dbbc640359
yeimsf <yeimsf@gmail.com>
2022-03-07 00:13:40 +0200

    Finished Encryption

--- securitylibrary/MainAlgorithms/PlayFair.cs
+++ securitylibrary/MainAlgorithms/PlayFair.cs
@@ -45,8 +45,6 @@ namespace SecurityLibrary
                 if (!alphaTaken[i])
                 {
                     map[it_row_ind, it_col_ind] = alphabet[i];
-                    int x = i;
-                    char a = alphabet[i];
                     alphaTaken[alphabet.IndexOf(alphabet[i])] = true;
                     if (it_col_ind == 4)
                         it_row_ind++;
@@ -84,151 +82,74 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, string key)
         {
-            //throw new NotImplementedException();
-            int current_row = 0;
-            int current_coulmn = 0;
+            int it_row_ind = 0, it_col_ind = 0;
+            char[,] map = new char[5, 5];
+            bool[] alphaTaken = new bool[25];
+            string C_T = "", alphabet = "abcdefghiklmnopqrstuvwxyz";
             plainText = plainText.ToLower();
-            int[,] arr = new int[5, 5];
-            int[] flag = new int[25];
-            string C_T = "";
-            string alphabet = "abcdefghiklmnopqrstuvwxyz";
+            List<Tuple<char, char>> P_T = new List<Tuple<char, char>>();
             for (int i = 0; i < key.Length; i++)
             {
-                if (key[i] == 'j')
+                if (!alphaTaken[alphabet.IndexOf(key[i])])
                 {
-                    if (current_coulmn == 4)
+                    if (key[i] == 'j')
                     {
-                        current_coulmn = 0;
-                        current_row++;
+                        alphaTaken[8] = true;
+                        map[it_row_ind, it_col_ind] = 'j';
+                        if (it_col_ind == 4)
+                            it_row_ind++;
+                        it_col_ind = (it_col_ind + 1) % 5;
                     }
                     else
-                        current_coulmn++;
-                    flag[8] = 1;
-                    arr[current_row, current_coulmn] = 8;
-                }
-                for (int j = 0; j <= 24; j++)
-                {
-                    if (key[i] == alphabet[j] && flag[j] == 0)
                     {
-                        arr[current_row, current_coulmn] = j;
-                        flag[j] = 1;
-                        if (current_coulmn == 4)
-                        {
-                            current_coulmn = 0;
-                            current_row++;
-                        }
-                        else
-                        {
-                            current_coulmn++;
-                            break;
-                        }
+                        map[it_row_ind, it_col_ind] = key[i];
+                        alphaTaken[alphabet.IndexOf(key[i])] = true;
+                        if (it_col_ind == 4)
+                            it_row_ind++;
+                        it_col_ind = (it_col_ind + 1) % 5;
                     }
                 }
             }
             for (int i = 0; i <= 24; i++)
             {
-                if (flag[i] == 0)
+                if (!alphaTaken[i])
                 {
-                    arr[current_row, current_coulmn] = i;
-                    flag[i] = 1;
-                    if (current_coulmn == 4)
-                    {
-                        current_coulmn = 0;
-                        current_row++;
-                    }
-                    else
-                        current_coulmn++;
+                    map[it_row_ind, it_col_ind] = alphabet[i];
+                    alphaTaken[alphabet.IndexOf(alphabet[i])] = true;
+                    if (it_col_ind == 4)
+                        it_row_ind++;
+                    it_col_ind = (it_col_ind + 1) % 5;
                 }
             }
-            string text = "";
-            text += plainText[0];
-            for (int i = 0; i < plainText.Length - 1; i++)
+            for (int i = 0; i < plainText.Length; i += 2)
             {
-                if (plainText[i + 1] != plainText[i])
-                    text += plainText[i + 1];
-                else
+                if(i == plainText.Length - 1)
+                    P_T.Add(new Tuple<char, char>(plainText[plainText.Length - 1], 'x'));
+                else if (plainText[i] == plainText[i + 1])
                 {
-                    if (i % 2 == 0)
-                    {
-                        text += 'x';
-                        text += plainText[i + 1];
-                    }
-                    else
-                    {
-                        text += plainText[i + 1];
-                    }
+                    P_T.Add(new Tuple<char, char>(plainText[i], 'x'));
+                    i--;
                 }
+                else
+                    P_T.Add(new Tuple<char, char>(plainText[i], plainText[i + 1]));
             }
-            if (text.Length % 2 != 0)
-                text += 'x';
-            int C_T_index = 0;
-            int frow = 0;
-            int fcol = 0;
-            int srow = 0;
-            int scol = 0;
-            for (int i = 0; i < text.Length - 1; i += 2)
+            Tuple<int, int> first_ind = new Tuple<int, int>(-1,-1), second_ind = new Tuple<int, int>(-1, -1);
+            foreach (var couple in P_T)
             {
-                for (int j = 0; j < 25; j++)
+                for (int i = 0; i < 5; i++)
                 {
-                    if (text[i] == alphabet[j])
-                    {
-                        C_T_index = j;
-                        for (int k = 0; k < 5; k++)
-                        {
-                            for (int l = 0; l < 5; l++)
-                            {
-                                if (alphabet[arr[k, l]] == alphabet[j])
-                                {
-                                    frow = k;
-                                    fcol = l;
-                                }
-                            }
-                        }
-                    }
-                    if (text[i + 1] == alphabet[j])
-                    {
-                        C_T_index = j;
-                        for (int k = 0; k < 5; k++)
-                        {
-                            for (int l = 0; l < 5; l++)
-                            {
-                                if (alphabet[arr[k, l]] == alphabet[j])
-                                {
-                                    srow = k;
-                                    scol = l;
-                                }
-                            }
-                        }
-                    }
-
+                    for (int j = 0; j < 5; j++)
+                        if (couple.Item1 == map[i, j])
+                            first_ind = new Tuple<int, int>(i, j);
+                        else if (couple.Item2 == map[i, j])
+                            second_ind = new Tuple<int, int>(i, j);
                 }
-                if (srow == frow)
-                {
-                    if (fcol == 4)
-                        C_T += alphabet[arr[frow, 0]];
-                    else
-                        C_T += alphabet[arr[frow, fcol + 1]];
-                    if (scol == 4)
-                        C_T += alphabet[arr[srow, 0]];
-                    else
-                        C_T += alphabet[arr[srow, scol + 1]];
-                }
-                else if (scol == fcol)
-                {
-                    if (frow == 4)
-                        C_T += alphabet[arr[0, fcol]];
-                    else
-                        C_T += alphabet[arr[frow + 1, fcol]];
-                    if (srow == 4)
-                        C_T += alphabet[arr[0, scol]];
-                    else
-                        C_T += alphabet[arr[srow + 1, scol]];
-                }
+                if (first_ind.Item2 == second_ind.Item2)
+                    C_T += map[(first_ind.Item1 + 1) % 5, first_ind.Item2].ToString() + map[(second_ind.Item1 + 1) % 5, second_ind.Item2].ToString();
+                else if (first_ind.Item1 == second_ind.Item1)
+                    C_T += map[first_ind.Item1, (first_ind.Item2 + 1) % 5].ToString() + map[second_ind.Item1, (second_ind.Item2 + 1) % 5].ToString();
                 else
-                {
-                    C_T += alphabet[arr[frow, scol]];
-                    C_T += alphabet[arr[srow, fcol]];
-                }
+                    C_T += map[first_ind.Item1, second_ind.Item2].ToString() + map[second_ind.Item1, first_ind.Item2].ToString();
             }
             C_T = C_T.ToUpper();
             return C_T;
