e145b4a6598911b03c8c12d68d8cf9fd1440898d
Youssefanter <yooiialy@gmail.com>
2022-03-09 16:27:12 +0200

    5alas fadel taka

--- securitylibrary/MainAlgorithms/Monoalphabetic.cs
+++ securitylibrary/MainAlgorithms/Monoalphabetic.cs
@@ -74,7 +74,44 @@ namespace SecurityLibrary
         /// <returns>Plain text</returns>
         public string AnalyseUsingCharFrequency(string cipher)
         {
-            throw new NotImplementedException();
+            char[] plainText = new char[cipher.Length];
+            double[] frequencyNumbers = { 12.51, 9.25, 8.04, 7.60, 7.26, 7.09, 6.54, 6.12, 5.49, 4.14, 3.99, 3.06, 2.71, 2.53, 2.30, 2.00, 1.96, 1.92, 1.73, 1.54, 0.99, 0.67, 0.19, 0.16, 0.11, 0.09 };
+            char[] freqLetters = new char[] { 'E', 'T', 'A', 'O', 'I', 'N', 'S', 'R', 'H', 'L', 'D', 'C', 'U', 'M', 'F', 'P', 'G', 'W', 'Y', 'B', 'V', 'K', 'X', 'J', 'Q', 'Z' };
+            double[] sortedFrequency = new double[26];
+            string alphabet = "abcdefghijklmnopqrstuvwxyz".ToUpper();
+            double counter = 0;
+            List<Tuple<char, double>> freqNumberAndLetter = new List<Tuple<char, double>>();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < cipher.Length; j++)
+                {
+                    if (alphabet[i] == cipher[j])
+                    {
+                        counter++;
+                    }
+                }
+                sortedFrequency[i] = (counter / cipher.Length) * 100;
+                counter = 0;
+            }
+            for (int i = 0; i < sortedFrequency.Length; i++)
+            {
+                freqNumberAndLetter.Add(new Tuple<char, double>(alphabet[i], sortedFrequency[i]));
+            }
+            var sortedFreqNumberAndLetter = freqNumberAndLetter.OrderByDescending(t => t.Item2).ToList();
+            for (int i = 0; i < sortedFreqNumberAndLetter.Count; i++)
+            {
+                for (int j = 0; j < cipher.Length; j++)
+                {
+                    if (sortedFreqNumberAndLetter[i].Item1 == cipher[j])
+                    {
+                        plainText[j] = freqLetters[i];
+                    }
+                }
+            }
+            string plainTextStr = new string(plainText);
+            plainTextStr = plainTextStr.ToUpper();
+            return plainTextStr;
+
         }
     }
 }
